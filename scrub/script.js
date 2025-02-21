0630c77b3a1471997d01a15403f957fc62132019
𝐀𝐌𝐀𝐍 𝐏𝐀𝐍𝐃𝐄𝐘. <69503838+aman-ap-official@users.noreply.github.com>
2025-02-21 22:43:35 +0530

    Add files via upload

---
+++ scrub/script.js
@@ -0,0 +1,14 @@
+gsap.from("#page2 #box",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start: "top 60%",
+        end:"top 30%",
+        scrub:2 // for scrole smoothness
+    }
+})
